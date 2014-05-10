842ecba0c24591cccf024b7b121ae5bb738a8614
Rodrigo <rodrigodemi@gmail.com>
2014-05-10 18:52:21 -0300

    inicio server rmi

---
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/business/MainBusiness.java
@@ -0,0 +1,15 @@
+package uy.edu.ort.arqliv.obligatorio.business;
+
+public class MainBusiness {
+
+	public static void main(String[] args) {
+		new MainBusiness().run();
+
+	}
+
+	private void run() {
+	
+		
+	}
+
+}
