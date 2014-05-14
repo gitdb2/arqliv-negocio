5b2394139898832d8e34718640f55897668ca3a2
Rodrigo <rodrigodemi@gmail.com>
2014-05-13 22:47:29 -0300

    mensaje de server online

--- src/main/java/uy/edu/ort/arqliv/obligatorio/business/MainBusiness.java
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/business/MainBusiness.java
@@ -1,8 +1,12 @@
 package uy.edu.ort.arqliv.obligatorio.business;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class MainBusiness {
 
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	public static void main(String[] args) {
 		
 		
@@ -11,7 +15,11 @@ public class MainBusiness {
 
 	private void run() {
 		ContextSingleton.getInstance().init();
-//		ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[] { "bean-remoting.xml", "bean-persistence.xml" });
+		log.info("=================================");
+		log.info("======== SERVER ONLINE ==========");
+		log.info("=================================");
+		
+		
 	}
 
 }
