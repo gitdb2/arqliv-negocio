948ee5f836014f9b99e3ff6b79f260eea2ab731f
mauricio <karbunkle@gmail.com>
2014-05-17 17:59:33 -0300

    reportes ok

--- src/main/java/uy/edu/ort/arqliv/obligatorio/business/services/ReportsServiceImpl.java
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/business/services/ReportsServiceImpl.java
@@ -1,5 +1,6 @@
 package uy.edu.ort.arqliv.obligatorio.business.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -9,6 +10,7 @@ import uy.edu.ort.arqliv.obligatorio.business.ContextSingleton;
 import uy.edu.ort.arqliv.obligatorio.common.ReportsService;
 import uy.edu.ort.arqliv.obligatorio.common.exceptions.CustomServiceException;
 import uy.edu.ort.arqliv.obligatorio.dominio.Arrival;
+import uy.edu.ort.arqliv.obligatorio.dominio.Container;
 import uy.edu.ort.arqliv.obligatorio.persistencia.constants.PersistenceConstants;
 import uy.edu.ort.arqliv.obligatorio.persistencia.dao.IArrivalDAO;
 
@@ -20,7 +22,11 @@ public class ReportsServiceImpl implements ReportsService {
 	public List<Arrival> arrivalsByMonth(String user, int month) throws CustomServiceException {
 		try {
 			IArrivalDAO arrivalDAO = (IArrivalDAO) ContextSingleton.getInstance().getBean(PersistenceConstants.ArrivalDao);
-			return arrivalDAO.arrivalsByMonth(month);
+			List<Arrival> ret = arrivalDAO.arrivalsByMonth(month);
+			for (Arrival arrival : ret) {
+				arrival.setContainers(new ArrayList<Container>(arrival.getContainers()));
+			}
+			return ret;
 		} catch (Exception e) {
 			log.error("Error al obtener el reporte de Arribos por Mes", e);
 			throw new CustomServiceException("", e);
@@ -31,7 +37,11 @@ public class ReportsServiceImpl implements ReportsService {
 	public List<Arrival> arrivalsByMonthByShip(String user, int month, Long shipId) throws CustomServiceException {
 		try {
 			IArrivalDAO arrivalDAO = (IArrivalDAO) ContextSingleton.getInstance().getBean(PersistenceConstants.ArrivalDao);
-			return arrivalDAO.arrivalsByMonthByShip(month, shipId);
+			List<Arrival> ret = arrivalDAO.arrivalsByMonthByShip(month, shipId);
+			for (Arrival arrival : ret) {
+				arrival.setContainers(new ArrayList<Container>(arrival.getContainers()));
+			}
+			return ret;
 		} catch (Exception e) {
 			log.error("Error al obtener el reporte de Arribos por Mes por Barco", e);
 			throw new CustomServiceException("", e);
