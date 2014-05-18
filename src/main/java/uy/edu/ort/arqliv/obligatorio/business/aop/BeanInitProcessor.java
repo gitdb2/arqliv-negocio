ea81c68afbf1d52905ad5424f2d3045e573e69a6
mauricio <karbunkle@gmail.com>
2014-05-18 20:38:25 -0300

    reemplace system.out por log

--- src/main/java/uy/edu/ort/arqliv/obligatorio/business/aop/BeanInitProcessor.java
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/business/aop/BeanInitProcessor.java
@@ -1,9 +1,13 @@
 package uy.edu.ort.arqliv.obligatorio.business.aop;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class BeanInitProcessor implements BeanPostProcessor {
+	
+	private final Logger log = LoggerFactory.getLogger(BeanInitProcessor.class);
 
 	@Override
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
@@ -12,7 +16,7 @@ public class BeanInitProcessor implements BeanPostProcessor {
 
 	@Override
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-		System.out.println("Se va a inicializar el bean " + beanName + " de la clase " + bean.getClass().getCanonicalName());
+		log.info("Se va a inicializar el bean " + beanName + " de la clase " + bean.getClass().getCanonicalName());
 		return bean;
 	}
 
