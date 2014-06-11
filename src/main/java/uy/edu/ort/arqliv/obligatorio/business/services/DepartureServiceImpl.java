3ef305b556c52264bd0d6954b072fb205d2f6c24
mauricio <karbunkle@gmail.com>
2014-06-11 01:48:46 -0300

    sin terminar partidas

--- src/main/java/uy/edu/ort/arqliv/obligatorio/business/services/DepartureServiceImpl.java
+++ src/main/java/uy/edu/ort/arqliv/obligatorio/business/services/DepartureServiceImpl.java
@@ -1,5 +1,6 @@
 package uy.edu.ort.arqliv.obligatorio.business.services;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,26 +11,86 @@ import uy.edu.ort.arqliv.obligatorio.business.ContextSingleton;
 import uy.edu.ort.arqliv.obligatorio.common.DepartureService;
 import uy.edu.ort.arqliv.obligatorio.common.exceptions.CustomInUseServiceException;
 import uy.edu.ort.arqliv.obligatorio.common.exceptions.CustomServiceException;
+import uy.edu.ort.arqliv.obligatorio.dominio.Arrival;
 import uy.edu.ort.arqliv.obligatorio.dominio.Container;
 import uy.edu.ort.arqliv.obligatorio.dominio.Departure;
+import uy.edu.ort.arqliv.obligatorio.dominio.Ship;
 import uy.edu.ort.arqliv.obligatorio.persistencia.constants.PersistenceConstants;
+import uy.edu.ort.arqliv.obligatorio.persistencia.dao.IArrivalDAO;
+import uy.edu.ort.arqliv.obligatorio.persistencia.dao.IContainerDAO;
 import uy.edu.ort.arqliv.obligatorio.persistencia.dao.IDepartureDAO;
+import uy.edu.ort.arqliv.obligatorio.persistencia.dao.IShipDAO;
 
 public class DepartureServiceImpl implements DepartureService {
 
 	private final Logger log = LoggerFactory.getLogger(DepartureServiceImpl.class);
 	
+	private SimpleDateFormat sdfOut = new SimpleDateFormat("dd/MM/yyyy");
+	
 	@Override
 	public long store(String user, Departure departure, Long shipId, List<Long> containerList) throws CustomServiceException {
+		return internalCreateUpdate(departure, shipId, containerList);
+	}
+	
+	private synchronized long internalCreateUpdate(Departure departure,	Long shipId, List<Long> containerList) throws CustomServiceException { 
+
 		try {
+			IContainerDAO containerDAO = (IContainerDAO) ContextSingleton.getInstance().getBean(PersistenceConstants.ContainerDao);
+			List<Container> containers = new ArrayList<Container>();
+
+			for (Long containerId : containerList) {
+				Container container = containerDAO.findById(containerId);
+
+				if (container == null) {
+					throw new CustomServiceException("el contenedor con id= " 
+							+ containerId + " no se encuentra en la DB. Se cancela el alta de partida");
+				}
+
+				if (containerDAO.isContainerInUse(containerId, departure.getDepartureDate())) {
+					throw new CustomServiceException("el contenedor con id= "
+							+ containerId
+							+ " ya partió para la fecha ("+ sdfOut.format(departure.getDepartureDate()) + ")."
+							+ " Se cancela el alta de partida");
+				}
+
+				containers.add(container);
+			}
+			
+			IShipDAO shipDAO = (IShipDAO) ContextSingleton.getInstance().getBean(PersistenceConstants.ShipDao);
+			Ship ship = shipDAO.findById(shipId);
+			if (ship == null) {
+				throw new CustomServiceException("el barco con id= "
+						+ shipId + " no se encuentra en la DB. Se cancela el alta de partida");
+			}
+			
+			IArrivalDAO arrivalDAO = (IArrivalDAO) ContextSingleton.getInstance().getBean(PersistenceConstants.ArrivalDao);
+			List<Arrival> arrivals = arrivalDAO.findArrivalByShipByDateByPort(ship.getId(), departure.getDepartureDate(), departure.getShipDestination());
+			if (arrivals.isEmpty()) {
+				throw new CustomServiceException("No hay arribos previos a la fecha " + sdfOut.format(departure.getDepartureDate()) 
+						+ " para el barco de id " + ship.getId()
+						+ " en el puerto " + departure.getShipDestination() + "."
+						+ " Se cancela el alta de partida");
+			}
+			
+			departure.setShip(ship);
+			departure.setContainers(containers);
+
 			IDepartureDAO departureDAO = (IDepartureDAO) ContextSingleton.getInstance().getBean(PersistenceConstants.DepartureDao);
+
 			return departureDAO.store(departure);
+		} catch (CustomServiceException e) {
+			log.error("error en alta de la Partida", e);
+			throw e;
 		} catch (Exception e) {
-			log.error("Error al dar de alta una Partida", e);
+			log.error("error en alta de la Partida", e);
 			throw new CustomServiceException(e.getMessage(), e);
 		}
 	}
 	
+	private long internalUpdate(Departure departure, Long shipId, List<Long> containerList) {
+		return 0;
+	}
+
 	@Override
 	public long update(String user, Departure departure, Long shipId, List<Long> containerList) throws CustomServiceException {
 		try {
@@ -82,7 +143,7 @@ public class DepartureServiceImpl implements DepartureService {
 			}
 			return ret;
 		} catch (Exception e) {
-			log.error("error al buscar un Arribo", e);
+			log.error("error al buscar una Partida", e);
 			throw new CustomServiceException(e.getMessage(), e);
 		}
 	}
